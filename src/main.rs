143f6bf40fd4379e89735c6610519d18faa96319
ron-png <41197190+ron-png@users.noreply.github.com>
2026-03-28 08:21:24 +0000

    possible fix

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("Failed to install rustls crypto provider");
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
